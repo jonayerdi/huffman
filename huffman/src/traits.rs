5bee164e43f1f5e73a60debc9e69c124f9ff8125
Jon Ayerdi <jon_ayerdi_33@hotmail.es>
2020-12-09 16:32:21 +0100

    impl Serialize for u8 arrays

--- huffman/src/traits.rs
+++ huffman/src/traits.rs
@@ -5,14 +5,14 @@ pub trait Serialize: Sized {
     fn deserialize<R: io::Read>(src: R) -> io::Result<Self>;
 }
 
-macro_rules! declare_serializable_integer {
+macro_rules! serializable_default {
     ($t: ty) => {
         impl Serialize for $t {
             fn serialize<W: io::Write>(&self, mut dst: W) -> io::Result<usize> {
-                dst.write(&self.to_le_bytes())
+                dst.write(&self.to_ne_bytes())
             }
             fn deserialize<R: io::Read>(mut src: R) -> io::Result<Self> {
-                let mut value: Self = 0;
+                let mut value: Self = Self::default();
                 unsafe {
                     let buffer = std::slice::from_raw_parts_mut(
                         &mut value as *mut Self as *mut u8,
@@ -26,7 +26,27 @@ macro_rules! declare_serializable_integer {
     };
 }
 
-declare_serializable_integer!(u8);
-declare_serializable_integer!(u16);
-declare_serializable_integer!(u32);
-declare_serializable_integer!(u64);
+serializable_default!(u8);
+serializable_default!(u16);
+serializable_default!(u32);
+serializable_default!(u64);
+
+macro_rules! serializable_u8_array {
+    ($sz: literal) => {
+        impl Serialize for [u8; $sz] {
+            fn serialize<W: io::Write>(&self, mut dst: W) -> io::Result<usize> {
+                dst.write(self)
+            }
+            fn deserialize<R: io::Read>(mut src: R) -> io::Result<Self> {
+                let mut value: Self = Self::default();
+                src.read_exact(&mut value)?;
+                Ok(value)
+            }
+        }
+    };
+}
+
+serializable_u8_array!(1);
+serializable_u8_array!(2);
+serializable_u8_array!(4);
+serializable_u8_array!(8);
